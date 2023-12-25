ad36d049804e143dbaf5a70197e5f6eccc3324c4
gameline <gameline_kood>
2023-12-25 10:41:08 +0200

    added ot_to_error

--- functions.go
+++ functions.go
@@ -30,3 +30,21 @@ func Warn(err error, message ...string) {
 		print_yellow(err, message)
 	}
 }
+func Rename_error(err error, message string) error {
+	if err != nil {
+		return fmt.Errorf(message)
+	}
+	return err
+}
+func ok_to_err(ok_or_not bool, message string) error {
+	if ok_or_not {
+		return fmt.Errorf(message)
+	}
+	return error(nil)
+}
+func nok_to_err(ok_or_not bool, message string) error {
+	if !ok_or_not {
+		return fmt.Errorf(message)
+	}
+	return error(nil)
+}
